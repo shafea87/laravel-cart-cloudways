f56a2f79721c588f727225e90a17790eabd19fa0
shafea87 <m.s.alsalkini@gmail.com>
2022-01-23 01:30:49 +0100

    First level
    First level

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -11,28 +11,31 @@
 </div>
 
 <div class="grid sm:grid-cols-4 gap-8 pt-20 mx-auto w-4/5">
-    <div class="mx-auto">
-        <img 
-            src="https://static.iphoned.nl/orca/products/9011/apple-macbook-pro-2021.png" 
-            alt="Apple MacBook Pro 2021" 
-            style="height:200px !important;">
+   @foreach ($products as $product)
+   <div class="mx-auto">
+    <img 
+        src="{{ asset($product->image_path) }}" 
+        alt="{{ $product->name }}" 
+        style="height:200px !important;">
 
-        <h2 class="text-xl text-gray-600 font-bold pb-8">
-            Apple MacBook Pro 2021
-        </h2>
-        
-        <p class="font-thin text-xs text-black pb-8">
-            Apple M1 Pro, 16 GPU, 16GB, 512 GB SSD
-        </p>
+    <h2 class="text-xl text-gray-600 font-bold pb-8">
+        {{ $product->name }}
+    </h2>
+    
+    <p class="font-thin text-xs text-black pb-8">
+        {{ $product->details }}
+    </p>
 
-        <p class="font-bold text-l text-black pb-8">
-           Price: <span class="text-red-500">$ 1993</span>
-        </p>
+    <p class="font-bold text-l text-black pb-8">
+       Price: <span class="text-red-500">$ {{ $product->price }} </span>
+    </p>
 
-        <a  href="/shop/1"
-            class="px-6 py-2 text-l uppercase text-white font-bold bg-blue-600 rounded-full w-full">
-            Read more...
-        </a>
-    </div>
+    <a  href="/shop/{{ $product->id }}"
+        class="px-6 py-2 text-l uppercase text-white font-bold bg-blue-600 rounded-full w-full">
+        Read more...
+    </a>
+</div>
+       
+   @endforeach
 </div>
 @endsection
--- resources/views/shop/show.blade.php
+++ resources/views/shop/show.blade.php
@@ -6,32 +6,32 @@
     <div class="grid sm:grid-cols-2 gap-2 pt-12 sm:pt-20 mx-auto w-4/5">
         <div class="mx-auto">
             <img 
-                src="https://static.iphoned.nl/orca/products/9011/apple-macbook-pro-2021.png" 
-                alt="Apple MacBook Pro 2021">
+                src="{{ asset($product->image_path) }}"  
+                alt="{{ $product->name }}">
         </div>
 
         <div>
             <h1 class="text-4xl text-gray-600 font-bold pb-8">
-                Apple MacBook Pro 2021
+                {{ $product->name }}
             </h1>
 
             <p class="font-bold text-l text-black pb-8">
-               Price: <span class="text-red-500">$ 1993</span>
+               Price: <span class="text-red-500">$ {{ $product->price }}</span>
             </p>
             
             <p class="font-bold text-l text-black pb-8">
-                Shipping: <span class="text-red-500">$ 25</span>
+                Shipping: <span class="text-red-500">$ {{ $product->shipping_cost }}</span>
             </p>
             
             <p class="font-thin text-s text-black pb-8">
-                Apple M1 Pro, 16 GPU, 16GB, 512 GB SSD
+                {{ $product->details }}
             </p>            
 
             <p class="text-gray-800 text-thin text-l leading-6 pb-12">
-                Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels. The Apple MacBook Pro packs 512GB of SSD storage. Connectivity options include Wi-Fi 802.11 ac, Bluetooth and it comes with 2 USB ports (2 x USB 3.0), Mic In ports.
+                {{ $product->description }}
             </p>
 
-            <a 
+            <a href="{{ route('add.to.cart',$product->id) }}"
                 class="px-10 py-6 text-l uppercase text-white font-bold bg-blue-600 rounded-full w-full" 
                 role="button" 
                 aria-pressed="true">
